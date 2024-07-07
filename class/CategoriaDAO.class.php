01865f92b1ce6155b8bdc8a451204489c72fdc87
Rafael <rafa101993@gmail.com>
2024-07-07 18:07:48 -0300

    Refactor: Cateoria class now use Conexao class

--- class/CategoriaDAO.class.php
+++ class/CategoriaDAO.class.php
@@ -1,39 +1,46 @@
 <?php
 	
+    include_once "Conexao.php";
+
 	class CategoriaDAO{
-        public $conexao;
+	    private Conexao $conexao;
+        private PDO $pdo;
 
 		public function __construct(){
-			$this->conexao = new PDO("mysql:host=localhost; port=3306;dbname=vagasdotcom", "root", "root");
+		    $this->conexao = new Conexao();
+			$this->pdo = new PDO(
+			    $this->conexao->getConn_srv(),
+			    $this->conexao->getConn_usr(),
+			    $this->conexao->getConn_pwd());
 		}
 		
         public function inserir(Categoria $c){
-            $sql = $this->conexao->prepare("INSERT INTO categoria (nome) VALUES (?)");
+            $sql = $this->pdo->prepare("INSERT INTO categoria (nome) VALUES (?)");
             $sql->bindValue(1, $c->getNome());
             return $sql->execute();
         }
 		
 		public function listar(){
-			$sql = $this->conexao->prepare("SELECT * FROM categoria");
+			$sql = $this->pdo->prepare("SELECT * FROM categoria");
 			$sql->execute();
 			return $sql->fetchAll();
 		}
 
         public function excluir(Categoria $c){
-            $sql = $this->conexao->prepare("DELETE FROM categoria WHERE id = ?");
+            $sql = $this->pdo->prepare("DELETE FROM categoria WHERE id = ?");
             $sql->bindValue(1, $c->getId());
             return $sql->execute();
         }
 
         public function editar(Categoria $c){
-            $sql = $this->conexao->prepare("UPDATE categoria SET nome = ? WHERE id = ?");
+            $sql = $this->pdo->prepare("UPDATE categoria SET nome = ? WHERE id = ?");
             $sql->bindValue(1, $c->getNome());
             $sql->bindValue(2, $c->getId());
             return $sql->execute();
         }
 
         public function listarPorId(Categoria $c){
-            $sql = $this->conexao->prepare("SELECT * FROM categoria WHERE id = ?");
+            $sql = $this->pdo->prepare("SELECT * FROM categoria WHERE id = ?");
             $sql->bindValue(1, $c->getId());
             $sql->execute();
             return $sql->fetch();
