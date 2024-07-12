903468ceb442a1dd8e5032c90e635608bf111a71
Rafael <rafa101993@gmail.com>
2024-07-12 13:15:12 -0300

    Update browse-job.php

--- browse-job.php
+++ browse-job.php
@@ -27,20 +27,20 @@
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
 						<i class="fa fa-bars"></i>
 					</button>
-					<a class="navbar-brand" href="index.html"><img src="img/logo.png" class="logo" alt=""></a>
+					<a class="navbar-brand" href="index.php"><img src="img/logo.png" class="logo" alt=""></a>
 				</div>
 				<!-- End Header Navigation -->
 
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="navbar-menu">
 					<ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
-							<li><a href="index.html">Home</a></li> 
+							<li><a href="index.php">Home</a></li>
 							<li><a href="usuario/login.php">Login</a></li>
 							<li><a href="companies.php">Companies</a></li>
 							<li class="dropdown">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Browse</a>
 								<ul class="dropdown-menu animated fadeOutUp" style="display: none; opacity: 1;">
-									<li class="active"><a href="browse-job.html">Browse Jobs</a></li>
+									<li class="active"><a href="browse-job.php">Browse Jobs</a></li>
 									<li><a href="company-detail.php">Job Detail</a></li>
 									<li><a href="resume.php">Resume Detail</a></li>
 								</ul>
