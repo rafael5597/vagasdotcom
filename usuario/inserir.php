423871e60ba8ae1fbfb6017a4ba3c647ae4ef7f1
Rafael <rafa101993@gmail.com>
2024-07-14 11:14:10 -0300

    Update inserir.php

--- usuario/inserir.php
+++ usuario/inserir.php
@@ -1,14 +1,161 @@
+<?php
+    include_once "../class/UsuarioDAO.class.php";
+
+    $objUsuarioDAO = new UsuarioDAO();
+
+    $msg = '';
+    $count = $objUsuarioDAO->contarUsuarios();
+    if(!$count > 0){
+        $msg = '<div class="alert alert-warning" role="alert">
+                    Atenção! Você é o PRIMEIRO usuário, portanto será ADM!
+                </div>';
+    }
+
+?>
 <!DOCTYPE html>
 <html>
     <head></head>
     <body>
-        <form action="inserir_ok.php" method="POST" enctype="multipart/form-data">
-            Nome:<input type="text" name="nome" required/><br />
-            Email:<input type="email" name="email" required/><br />
-            Senha:<input type="password" name="senha" required/><br />
-            LinkedIn:<input type="text" name="link_linkedin" required>
-            Foto:<input type="file" name="foto" required/><br />
-            <button type="submit">Enviar</button>
-        </form>
+
     </body>
+</html>
+
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Vagas.Com | Seu novo emprego está aqui!</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- All Plugin Css -->
+    <link rel="stylesheet" href="../css/plugins.css">
+
+    <!-- Style & Common Css -->
+    <link rel="stylesheet" href="../css/common.css">
+    <link rel="stylesheet" href="../css/main.css">
+
+</head>
+
+<body>
+
+<!-- Navigation Start  -->
+<nav class="navbar navbar-default navbar-sticky bootsnav">
+
+    <div class="container">
+        <!-- Start Header Navigation -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="index.php"><img src="../img/logo.png" class="logo" alt=""></a>
+        </div>
+        <!-- End Header Navigation -->
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbar-menu">
+            <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="../companies.php">Companies</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Browse</a>
+                    <ul class="dropdown-menu animated fadeOutUp" style="display: none; opacity: 1;">
+                        <li class="active"><a href="../browse-job.php">Browse Jobs</a></li>
+                        <li><a href="../company-detail.php">Job Detail</a></li>
+                        <li><a href="../resume.php">Resume Detail</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div>
+</nav>
+<!-- Navigation End  -->
+
+<!-- login section start -->
+<section class="login-wrapper">
+    <div class="container">
+        <div class="col-md-6 col-sm-8 col-md-offset-3 col-sm-offset-2">
+            <img class="img-responsive" alt="logo" src="../img/logo.png">
+            <?=$msg;?>
+            <form action="inserir_ok.php" method="POST" enctype="multipart/form-data">
+                <input type="text" name="nome" class="form-control input-lg" placeholder="Nome" required/><br />
+                <input type="email" name="email" class="form-control input-lg" placeholder="E-mail" required/><br />
+                <input type="password" name="senha" class="form-control input-lg" placeholder="Senha" required/><br />
+                <input type="text" name="link_linkedin" class="form-control input-lg" placeholder="LinkedIn" required>
+                <label for="foto">Selecione uma foto:</label><input type="file" name="foto" id="foto" class="form-control input-lg" required/><br />
+                <button type="submit" class="btn btn-success">REGISTRAR</button>
+            </form>
+        </div>
+    </div>
+</section>
+<!-- login section End -->
+
+<!-- footer start -->
+<footer>
+    <div class="container">
+        <!-- <div class="col-md-3 col-sm-6">
+            <h4>Featured Job</h4>
+            <ul>
+                <li><a href="#">Browse Jobs</a></li>
+                <li><a href="#">Premium MBA Jobs</a></li>
+                <li><a href="#">Access Database</a></li>
+                <li><a href="#">Manage Responses</a></li>
+                <li><a href="#">Report a Problem</a></li>
+                <li><a href="#">Mobile Site</a></li>
+                <li><a href="#">Jobs by Skill</a></li>
+            </ul>
+        </div>
+
+        <<div class="col-md-3 col-sm-6">
+            <h4>Latest Job</h4>
+            <ul>
+                <li><a href="#">Browse Jobs</a></li>
+                <li><a href="#">Premium MBA Jobs</a></li>
+                <li><a href="#">Access Database</a></li>
+                <li><a href="#">Manage Responses</a></li>
+                <li><a href="#">Report a Problem</a></li>
+                <li><a href="#">Mobile Site</a></li>
+                <li><a href="#">Jobs by Skill</a></li>
+            </ul>
+        </div> -->
+
+        <!-- <div class="col-md-3 col-sm-6">
+            <h4>Reach Us</h4>
+            <address>
+            <ul>
+            <li>1201, Murakeu Market, QUCH07<br>
+            United Kingdon</li>
+            <li>Email: Support@job.com</li>
+            <li>Call: 044 123 458 65879</li>
+            <li>Skype: job@skype</li>
+            <li>FAX: 123 456 85</li>
+            </ul>
+            </address>
+        </div> -->
+
+        <!-- <div class="col-md-3 col-sm-6">
+            <h4>Drop A Mail</h4>
+            <form>
+                <input type="text" class="form-control input-lg" placeholder="Your Name">
+                <input type="text" class="form-control input-lg" placeholder="Email...">
+                <textarea class="form-control" placeholder="Message"></textarea>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div> -->
+
+
+    </div>
+    <div class="copy-right">
+        <p>&copy;Copyright 2024 Vagas.Com | Seu novo emprego está aqui!</p>
+    </div>
+</footer>
+
+<script type="text/javascript" src="../js/jquery.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../js/owl.carousel.min.js"></script>
+<script src="../js/bootsnav.js"></script>
+<script src="../js/main.js"></script>
+</body>
 </html>
