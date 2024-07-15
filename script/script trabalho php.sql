432af34c745ef19aa88b47617706ad2bec3b78ab
Rafael <rafa101993@gmail.com>
2024-07-15 02:43:19 -0300

    Update sql script

--- script/script trabalho php.sql
+++ script/script trabalho php.sql
@@ -24,17 +24,27 @@ CREATE TABLE `empresa` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `candidatura` (
+    `id_candidato` INT NOT NULL,
+    `id_vaga` INT NOT NULL,
+    `data_candidatura` DATETIME NOT NULL,
+    PRIMARY KEY (`id_candidato`, `id_vaga`),
+    FOREIGN KEY (`id_candidato`) REFERENCES `usuario`(`id`),
+    FOREIGN KEY (`id_vaga`) REFERENCES `vaga`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `vaga` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `titulo` VARCHAR(255) NOT NULL,
     `descricao` TEXT NOT NULL,
-    `cargo` VARCHAR(255) NOT NULL,
+    `cargo` VARCHAR(255),
     `modalidade` VARCHAR(255) NOT NULL,
     `localizacao` VARCHAR(255) NOT NULL,
     `empresa_id` INT NOT NULL,
     `categoria_id` INT NOT NULL,
     `data_pulicacao` DATETIME NOT NULL,
     `ativo` BOOLEAN NOT NULL DEFAULT 1,
+    `imagem` VARCHAR(255),
     PRIMARY KEY (`id`),
     FOREIGN KEY (`categoria_id`) REFERENCES `categoria`(`id`),
     FOREIGN KEY (`empresa_id`) REFERENCES `empresa`(`id`)
