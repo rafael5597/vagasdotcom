83393de9fe4feaaf92b967320f6d4468307738f9
Rafael <rafa101993@gmail.com>
2024-07-16 02:40:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,74 @@
 <?php
-    session_start();
+session_start();
 
-    include_once "class/Usuario.class.php";
-    include_once "class/UsuarioDAO.class.php";
+include_once "class/Usuario.class.php";
+include_once "class/UsuarioDAO.class.php";
+include_once "class/VagaDAO.class.php";
+include_once "class/EmpresaDAO.class.php";
+include_once "class/Empresa.class.php";
+include_once "class/CategoriaDAO.class.php";
+include_once "class/Categoria.class.php";
 
-    if(!isset($_SESSION['logado'])){
-        header("location:usuario/login.php");
-    } else {
+if(!isset($_SESSION['logado'])){
+    header("location:usuario/login.php");
+} else {
 
-        $objUsuario = new Usuario();
-        $objUsuarioDAO = new UsuarioDAO();
-        $objUsuario->setId($_SESSION['idUsuario']);
-        $retorno = $objUsuarioDAO->listarUmUsuario($objUsuario);
+    $objUsuario = new Usuario();
+    $objUsuarioDAO = new UsuarioDAO();
+    $objUsuario->setId($_SESSION['idUsuario']);
+    $retorno = $objUsuarioDAO->listarUmUsuario($objUsuario);
 
-        if(!$retorno){
-            header('location: usuario/logout.php');
-        }
-
-        $nomeUsuario = $_SESSION['nomeUsuario'];
-        if($_SESSION['admin']){
-            header('location: usuario/adm/');
-        }
+    if(!$retorno){
+        header('location: usuario/logout.php');
     }
+
+    $nomeUsuario = $_SESSION['nomeUsuario'];
+    if($_SESSION['admin']){
+        header('location: usuario/adm/');
+    }
+}
+
+$objVagaDAO = new VagaDAO();
+$qtdVagas = $objVagaDAO->contarVagas();
+
+$objEmpresaDAO = new EmpresaDAO();
+$objEmpresa = new Empresa();
+$qtdEmpresas = $objEmpresaDAO->contarEmpresas();
+
+$objCategoriaDAO = new CategoriaDAO();
+$objCategoria = new Categoria();
+
+$qtdUsuarios = $objUsuarioDAO->contarUsuarios();
+
+$vagaHtml = '';
+$vagas = $objVagaDAO->listar();
+
+foreach ($vagas as $vaga){
+    $objCategoria->setId($vaga["categoria_id"]);
+    $cat = $objCategoriaDAO->listarPorId($objCategoria);
+
+    $objEmpresa->setId($vaga["empresa_id"]);
+    $emp = $objEmpresaDAO->listarPorId($objEmpresa);
+
+    $vagaHtml .= '
+    <div class="company-list">
+        <div class="row">
+            <div class="col-md-2 col-sm-2">
+                <div class="company-logo">
+                    <img src="img/wipro.png" class="img-responsive" alt="" />
+                </div>
+            </div>
+            <div class="col-md-10 col-sm-10">
+                <div class="company-content">
+                    <h3>'.$vaga["titulo"].'<span class="internship">'.$cat["nome"].'</span></h3>
+                        <p><span class="company-name"><i class="fa fa-briefcase"></i>'.$emp["nome"].'</span><span class="company-location"><i class="fa fa-map-marker"></i>'.$vaga["localizacao"].'</span><span class="package"><i class="fa fa-money"></i>$24,000-$52,000</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+';
+}
+
 
 ?>
 
@@ -62,7 +109,6 @@
         <div class="collapse navbar-collapse" id="navbar-menu">
             <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
                 <li><a href="index.php">Home</a></li>
-                <li><a href="usuario/login.php">Login</a></li>
                 <li><a href="companies.php">Companies</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Browse</a>
@@ -128,181 +174,43 @@
     </div>
 </section>
 
-<section class="features">
-    <div class="container">
-        <div class="col-md-4 col-sm-4">
-            <div class="features-content">
-                <span class="box1"><span aria-hidden="true" class="icon-dial"></span></span>
-                <h3>Create An Account</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
-            </div>
-        </div>
-
-        <div class="col-md-4 col-sm-4">
-            <div class="features-content">
-                <span class="box1"><span aria-hidden="true" class="icon-search"></span></span>
-                <h3>Search Desired Job</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
-            </div>
-        </div>
-
-        <div class="col-md-4 col-sm-4">
-            <div class="features-content">
-                <span class="box1"><span aria-hidden="true" class="icon-printer"></span></span>
-                <h3>Send Your Resume</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
-            </div>
-        </div>
-
-        <div class="col-md-4 col-sm-4">
-            <div class="features-content">
-                <span class="box1"><span aria-hidden="true" class="icon-dial"></span></span>
-                <h3>Create An Account</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
-            </div>
-        </div>
-
-        <div class="col-md-4 col-sm-4">
-            <div class="features-content">
-                <span class="box1"><span aria-hidden="true" class="icon-search"></span></span>
-                <h3>Search Desired Job</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
-            </div>
-        </div>
-
-        <div class="col-md-4 col-sm-4">
-            <div class="features-content">
-                <span class="box1"><span aria-hidden="true" class="icon-printer"></span></span>
-                <h3>Send Your Resume</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid ut labore et dolore magna aliqua.</p>
-            </div>
-        </div>
-
-    </div>
-</section>
-
 <section class="counter">
     <div class="container">
-        <div class="col-md-3 col-sm-3">
+        <div class="col-md-4 col-sm-4">
             <div class="counter-text">
                 <span aria-hidden="true" class="icon-briefcase"></span>
-                <h3>1000</h3>
-                <p>Jobs Posted</p>
+                <h3><?=$qtdVagas;?></h3>
+                <p>Vagas Postadas</p>
             </div>
         </div>
 
-        <div class="col-md-3 col-sm-3">
+        <div class="col-md-4 col-sm-4">
             <div class="counter-text">
                 <span class="box1"><span aria-hidden="true" class="icon-expand"></span></span>
-                <h3>207</h3>
-                <p>All Companies</p>
+                <h3><?=$qtdEmpresas;?></h3>
+                <p>Empresas Contratando</p>
             </div>
         </div>
 
-        <div class="col-md-3 col-sm-3">
+        <div class="col-md-4 col-sm-4">
             <div class="counter-text">
                 <span class="box1"><span aria-hidden="true" class="icon-profile-male"></span></span>
-                <h3>700+</h3>
-                <p>Total Members</p>
+                <h3><?=$qtdUsuarios;?>+</h3>
+                <p>Total de usuários</p>
             </div>
         </div>
 
-        <div class="col-md-3 col-sm-3">
-            <div class="counter-text">
-                <span class="box1"><span aria-hidden="true" class="icon-sad"></span></span>
-                <h3>802+</h3>
-                <p>Happy Members</p>
-            </div>
-        </div>
     </div>
 </section>
 
 <section class="jobs">
     <div class="container">
         <div class="row heading">
-            <h2>Find Popular Jobs</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</p>
+            <h2>Encontre vagas populares</h2>
+            <p>As melhores empresas que você pode trabalhar estão aqui!</p>
         </div>
         <div class="companies">
-            <div class="company-list">
-                <div class="row">
-                    <div class="col-md-2 col-sm-2">
-                        <div class="company-logo">
-                            <img src="img/google.png" class="img-responsive" alt="" />
-                        </div>
-                    </div>
-                    <div class="col-md-10 col-sm-10">
-                        <div class="company-content">
-                            <h3>IOS Developer<span class="full-time">Full Time</span></h3>
-                            <p><span class="company-name"><i class="fa fa-briefcase"></i>Google</span><span class="company-location"><i class="fa fa-map-marker"></i> 07th Avenue, New York, NY, United States</span><span class="package"><i class="fa fa-money"></i>$22,000-$50,000</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="company-list">
-                <div class="row">
-                    <div class="col-md-2 col-sm-2">
-                        <div class="company-logo">
-                            <img src="img/microsoft.png" class="img-responsive" alt="" />
-                        </div>
-                    </div>
-                    <div class="col-md-10 col-sm-10">
-                        <div class="company-content">
-                            <h3>Back-End developer<span class="part-time">Part Time</span></h3>
-                            <p><span class="company-name"><i class="fa fa-briefcase"></i>Microsoft</span><span class="company-location"><i class="fa fa-map-marker"></i> 7th Avenue, New York, NY, United States</span><span class="package"><i class="fa fa-money"></i>$20,000-$52,000</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="company-list">
-                <div class="row">
-                    <div class="col-md-2 col-sm-2">
-                        <div class="company-logo">
-                            <img src="img/apple.png" class="img-responsive" alt="" />
-                        </div>
-                    </div>
-                    <div class="col-md-10 col-sm-10">
-                        <div class="company-content">
-                            <h3>UI/UX Designer<span class="freelance">Freelance</span></h3>
-                            <p><span class="company-name"><i class="fa fa-briefcase"></i>Apple</span><span class="company-location"><i class="fa fa-map-marker"></i> 7th Avenue, New York, NY, United States</span><span class="package"><i class="fa fa-money"></i>$22,000-$50,000</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="company-list">
-                <div class="row">
-                    <div class="col-md-2 col-sm-2">
-                        <div class="company-logo">
-                            <img src="img/wipro.png" class="img-responsive" alt="" />
-                        </div>
-                    </div>
-                    <div class="col-md-10 col-sm-10">
-                        <div class="company-content">
-                            <h3>IOS developer<span class="internship">Intership</span></h3>
-                            <p><span class="company-name"><i class="fa fa-briefcase"></i>Wipro</span><span class="company-location"><i class="fa fa-map-marker"></i> 8th Avenue, New York, NY, United States</span><span class="package"><i class="fa fa-money"></i>$24,000-$52,000</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="company-list">
-                <div class="row">
-                    <div class="col-md-2 col-sm-2">
-                        <div class="company-logo">
-                            <img src="img/twitter.png" class="img-responsive" alt="" />
-                        </div>
-                    </div>
-                    <div class="col-md-10 col-sm-10">
-                        <div class="company-content">
-                            <h3>Marketing Holder<span class="full-time">Full Time</span></h3>
-                            <p><span class="company-name"><i class="fa fa-briefcase"></i>Twitter</span><span class="company-location"><i class="fa fa-map-marker"></i> 4th Avenue, New York, NY, United States</span><span class="package"><i class="fa fa-money"></i>$24,000-$48,000</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?=$vagaHtml?>
         </div>
         <div class="row">
             <input type="button" class="btn brows-btn" value="Brows All Jobs" />
@@ -359,74 +267,6 @@
                         <span class="post">Web Designer</span>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="pricind">
-    <div class="container">
-        <div class="col-md-4 col-sm-4">
-            <div class="package-box">
-                <div class="package-header">
-                    <i class="fa fa-cog" aria-hidden="true"></i>
-                    <h3>Professional</h3>
-                </div>
-                <div class="package-info">
-                    <ul>
-                        <li>3 Designs</li>
-                        <li>3 PSD Designs</li>
-                        <li>4 color Option</li>
-                        <li>10GB Disk Space</li>
-                        <li>Full Support</li>
-                    </ul>
-                </div>
-                <div class="package-price">
-                    <h2><sup>$ </sup>10<sub>/Month</sub></h2>
-                </div>
-                <button type="submit" class="btn btn-package">Sign Up</button>
-            </div>
-        </div>
-        <div class="col-md-4 col-sm-4">
-            <div class="package-box">
-                <div class="package-header">
-                    <i class="fa fa-star-half-o" aria-hidden="true"></i>
-                    <h3>Standard</h3>
-                </div>
-                <div class="package-info">
-                    <ul>
-                        <li>3 Designs</li>
-                        <li>3 PSD Designs</li>
-                        <li>4 color Option</li>
-                        <li>10GB Disk Space</li>
-                        <li>Full Support</li>
-                    </ul>
-                </div>
-                <div class="package-price">
-                    <h2><sup>$ </sup>110<sub>/Month</sub></h2>
-                </div>
-                <button type="submit" class="btn btn-package">Sign Up</button>
-            </div>
-        </div>
-        <div class="col-md-4 col-sm-4">
-            <div class="package-box">
-                <div class="package-header">
-                    <i class="fa fa-cube" aria-hidden="true"></i>
-                    <h3>Premium</h3>
-                </div>
-                <div class="package-info">
-                    <ul>
-                        <li>3 Designs</li>
-                        <li>3 PSD Designs</li>
-                        <li>4 color Option</li>
-                        <li>10GB Disk Space</li>
-                        <li>Full Support</li>
-                    </ul>
-                </div>
-                <div class="package-price">
-                    <h2><sup>$ </sup>170<sub>/Month</sub></h2>
-                </div>
-                <button type="submit" class="btn btn-package">Sign Up</button>
             </div>
         </div>
     </div>
